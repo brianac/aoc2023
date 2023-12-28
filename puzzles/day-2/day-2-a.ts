0c4fef09c8a74abbe941ef3006a2f7e4802bfcbf
Briana Coppard <bcoppard@zendesk.com>
2023-12-28 18:48:36 +1100

    day 2 done

--- puzzles/day-2/day-2-a.ts
+++ puzzles/day-2/day-2-a.ts
@@ -3,7 +3,38 @@ import chalk from 'chalk';
 
 export async function day2a(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  const possibleVals = {
+    red: 12,
+    green: 13,
+    blue: 14
+  }
+
+  const total = data.reduce((acc, game) => {
+    const parts = game.split(':')
+    const id = parts[0].split(' ')[1]
+    const pulls = parts[1].split(';')
+
+    const valid = pulls.map((pull) => {      
+      return pull.split(',').map((p) => {
+        const s = p.split(' ')
+        const num = s[1]
+        const color = s[2]
+
+        if (num > possibleVals[color]) {
+          return false
+        }
+        return true
+      })
+    }).flat().filter(e => e === false)
+
+    if (!valid.length) {
+      return acc + Number(id)
+    }
+    return acc
+  }, 0)
+
+  return total;
 }
 
 const answer = await day2a();
--- puzzles/day-2/day-2-b.ts
+++ puzzles/day-2/day-2-b.ts
@@ -3,7 +3,34 @@ import chalk from 'chalk';
 
 export async function day2b(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  const total = data.reduce((acc, game) => {
+    const parts = game.split(':')
+    const id = parts[0].split(' ')[1]
+    const pulls = parts[1].split(';')
+    
+    const highest = {
+      red: 0,
+      blue: 0,
+      green: 0
+    }
+
+    pulls.forEach((pull) => {      
+      pull.split(',').forEach((p) => {
+        const s = p.split(' ')
+        const num = s[1]
+        const color = s[2]
+
+        if (highest[color] < num) {
+          highest[color] = Number(num)
+        }
+      })
+    })
+
+    return acc + highest.red*highest.green*highest.blue
+  }, 0)
+
+  return total;
 }
 
 const answer = await day2b();
