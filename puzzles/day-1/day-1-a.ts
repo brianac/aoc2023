eb317edb38e24bf5e2c71c5511a2872a545c2e8e
Briana Coppard <bcoppard@zendesk.com>
2023-12-28 17:28:38 +1100

    day1 done

--- puzzles/day-1/day-1-a.ts
+++ puzzles/day-1/day-1-a.ts
@@ -3,7 +3,25 @@ import chalk from 'chalk';
 
 export async function day1a(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  const numbers = data.map((line) => {
+    let firstNum: string | undefined = undefined
+    let secondNum: string | undefined = undefined
+
+    line.split('').forEach((char) => {
+      if (Number(char)) {
+        if (!firstNum) {
+          firstNum = char
+        }
+
+        secondNum = char
+      }
+    })
+
+    return firstNum + secondNum
+  })
+
+  return numbers.reduce((a,b)=>Number(a)+Number(b), 0);
 }
 
 const answer = await day1a();
--- puzzles/day-1/day-1-b.ts
+++ puzzles/day-1/day-1-b.ts
@@ -1,9 +1,42 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+const map = {
+  one: '1',
+  two: '2', 
+  three: '3',
+  four: '4',
+  five: '5',
+  six: '6', 
+  seven: '7',
+  eight: '8',
+  nine: '9',
+  1: '1',
+  2: '2',
+  3: '3',
+  4: '4',
+  5: '5',
+  6: '6',
+  7: '7',
+  8: '8',
+  9: '9',
+}
+
 export async function day1b(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  const numbers = data.map((line) => {
+    let matches = []
+
+    Object.keys(map).forEach(el => {
+      matches.push(...line.matchAll(new RegExp(el, "g")))
+    })
+    matches.sort((a, b) => a.index - b.index);
+
+    return map[matches[0][0]] + map[matches[matches.length-1][0]]
+  })
+
+  return numbers.reduce((a,b)=>Number(a)+Number(b), 0);
 }
 
 const answer = await day1b();
